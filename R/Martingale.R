6b1a48220cba9338e97b37dd0ae7a0dbff083116
Jeremy Yee <jeremyyee@outlook.com.au>
2017-04-19 12:36:16 +1000

    fixed dimensions for random control

--- R/Martingale.R
+++ R/Martingale.R
@@ -27,9 +27,10 @@ Martingale <- function(value, disturb, weight, path, control) {
     if (length(dim(control)) == 2) {  ## 3 dim if full control
         return(output)
     } else if (length(dim(control)) == 3) {  ## 4 dim if partial control
+        n_dec <- v_dims[4]
         n_pos <- v_dims[3]
-        n_path <- path_dims[2]
-        return(array(output, dim = c(dim(control)[1], dim(control)[2],
-                                     n_pos, n_path)))
+        n_path <- dim(path)[2]
+        return(array(output, dim = c(n_dec - 1, dim(control)[2],
+                                     n_path, n_pos)))
     }
 }
